099612d6830e9bb10fd953d142737750dc1d2bfd
abhinavanand-fed <abhinavanand080@gmail.com>
2023-02-04 02:09:08 +0530

    Not complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./Todo.css";
 
-function App() {
+function TodoApp() {
+  const [todos, setTodos] = useState([
+    { text: "Learn React", isCompleted: false },
+    { text: "Learn CSS", isCompleted: false },
+    { text: "Learn HTML", isCompleted: false }
+  ]);
+  const [input, setInput] = useState("");
+
+  const handleInput = event => {
+    setInput(event.target.value);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    addTodo(input);
+    setInput("");
+  };
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  };
+
+  const editTodo = (index, text) => {
+    const newTodos = [...todos];
+    newTodos[index].text = text;
+    setTodos(newTodos);
+  };
+
+  const deleteTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo-app">
+      <div className="header">
+        <form onSubmit={handleSubmit}>
+          <input type="text" value={input} onChange={handleInput} placeholder="Add a todo" />
+          <button type="submit">Add Todo</button>
+        </form>
+      </div>
+      <div className="todos">
+        {todos.map((todo, index) => (
+          <div className="todo" key={index}>
+            <div
+              className={`todo-text ${todo.isCompleted ? "completed" : ""}`}
+            >
+              {todo.text}
+            </div>
+            <div className="buttons">
+              <button onClick={() => completeTodo(index)}>Complete</button>
+              <button onClick={() => editTodo(index, "Edited Todo")}>
+                Edit
+              </button>
+              <button onClick={() => deleteTodo(index)}>Delete</button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
 
-export default App;
+export default TodoApp;
